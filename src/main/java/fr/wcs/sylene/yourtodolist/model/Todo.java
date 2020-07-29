0993833c688c47536b8c1b5d0f4fb3347c16573c
Sylène <sylene.manusset@gmail.com>
2020-07-29 09:51:50 +0200

    add Todo model and the corresponding repository which extend JpaRepository

---
+++ src/main/java/fr/wcs/sylene/yourtodolist/model/Todo.java
@@ -0,0 +1,36 @@
+package fr.wcs.sylene.yourtodolist.model;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Type(type = "uuid-char")
+    private UUID uuid;
+
+    @Column(name = "content")
+    private String content;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
